049acfd0811daeccea61335c07c054b83409385a
Hyun-je <bigae2@gmail.com>
2024-09-04 22:19:59 +0900

    fix error

--- models/Autoformer_Multiscale.py
+++ models/Autoformer_Multiscale.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Autoformer import Model as Autoformer
+from models.Autoformer import Model as Autoformer
 import math
 import numpy as np
 
@@ -16,18 +16,18 @@ class Model(nn.Module):
         self.pred_len = configs.pred_len
 
         self.downsample_1_to_2 = nn.Sequential(
-            nn.Conv1d(configs.c_in, configs.c_in, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm1d(configs.c_in),
+            nn.Conv1d(configs.enc_in, configs.enc_in, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm1d(configs.enc_in),
             nn.ReLU(),
         )
         self.downsample_2_to_4 = nn.Sequential(
-            nn.Conv1d(configs.c_in, configs.c_in, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm1d(configs.c_in),
+            nn.Conv1d(configs.enc_in, configs.enc_in, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm1d(configs.enc_in),
             nn.ReLU(),
         )
         self.downsample_4_to_8 = nn.Sequential(
-            nn.Conv1d(configs.c_in, configs.c_in, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm1d(configs.c_in),
+            nn.Conv1d(configs.enc_in, configs.enc_in, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm1d(configs.enc_in),
             nn.ReLU(),
         )
         
@@ -52,17 +52,17 @@ class Model(nn.Module):
         self.sub_model4 = Autoformer(configs)
 
         self.upsample_8_to_4 = nn.Sequential(
-            nn.ConvTranspose1d(configs.c_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose1d(configs.enc_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm1d(configs.c_out),
             nn.ReLU(),
         )
         self.upsample_4_to_2 = nn.Sequential(
-            nn.ConvTranspose1d(configs.c_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose1d(configs.enc_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm1d(configs.c_out),
             nn.ReLU(),
         )
         self.upsample_2_to_1 = nn.Sequential(
-            nn.ConvTranspose1d(configs.c_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose1d(configs.enc_in, configs.c_out, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm1d(configs.c_out),
             nn.ReLU(),
         )
@@ -70,13 +70,17 @@ class Model(nn.Module):
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
 
         x1 = x_enc
-        x2 = self.downsample_1_to_2(x_enc)
-        x4 = self.downsample_2_to_4(x_enc)
-        x8 = self.downsample_4_to_8(x_enc)
+        x2 = self.downsample_1_to_2(x_enc.permute(0, 2, 1)).permute(0, 2, 1)
+        x4 = self.downsample_2_to_4(x_enc.permute(0, 2, 1)).permute(0, 2, 1)
+        x8 = self.downsample_4_to_8(x_enc.permute(0, 2, 1)).permute(0, 2, 1)
         
-        x8 = self.sub_model4(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
-        x4 = self.upsample_8_to_4(x8) + self.sub_model1(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
-        x2 = self.upsample_4_to_2(x4) + self.sub_model2(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
-        x1 = self.upsample_2_to_1(x2) + self.sub_model3(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
-        
-        return x1
+        x8_enc = self.sub_model4(x8, x_mark_enc, x_dec, x_mark_dec, mask)
+        x4_enc = self.sub_model3(x4, x_mark_enc, x_dec, x_mark_dec, mask)
+        x2_enc = self.sub_model2(x2, x_mark_enc, x_dec, x_mark_dec, mask)
+        x1_enc = self.sub_model1(x1, x_mark_enc, x_dec, x_mark_dec, mask)
+
+        x4_up = x4_enc + self.upsample_8_to_4(x8_enc.permute(0, 2, 1)).permute(0, 2, 1)
+        x2_up = x2_enc + self.upsample_4_to_2(x4_enc.permute(0, 2, 1)).permute(0, 2, 1)
+        x1_up = x1_enc + self.upsample_2_to_1(x2_enc.permute(0, 2, 1)).permute(0, 2, 1)
+
+        return x1_up
