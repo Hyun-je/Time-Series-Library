440242622f21ac44e08db2c16c354c7a6dedd4d4
Hyun-je <bigae2@gmail.com>
2024-09-08 20:01:01 +0900

    Refactor forward method in MultiEnsemble.py to include an optional mask parameter

--- models/MultiEnsemble.py
+++ models/MultiEnsemble.py
@@ -60,10 +60,10 @@ class Model(torch.nn.Module):
         x_enc_4 = x_enc[:, :, ::4]   # (B, 51, L//4)
         x_enc_8 = x_enc[:, :, ::8]   # (B, 51, L//8)
 
-        out_1 = self.forcast_1(x_enc_1)   # (B, 51, pred_len)
-        out_2 = self.forcast_2(x_enc_2)   # (B, 51, pred_len)
-        out_4 = self.forcast_4(x_enc_4)   # (B, 51, pred_len)
-        out_8 = self.forcast_8(x_enc_8)   # (B, 51, pred_len)
+        out_1 = self.forcast_1(x_enc_1, None, None, None, None)   # (B, 51, pred_len)
+        out_2 = self.forcast_2(x_enc_2, None, None, None, None)   # (B, 51, pred_len)
+        out_4 = self.forcast_4(x_enc_4, None, None, None, None)   # (B, 51, pred_len)
+        out_8 = self.forcast_8(x_enc_8, None, None, None, None)   # (B, 51, pred_len)
 
         out = (out_1 * 0.5) + (out_2 * 0.3) + (out_4 * 0.2) + (out_8 * 0.1)      # (B, 51, pred_len)
         
